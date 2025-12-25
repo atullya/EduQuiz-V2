e5cca35473612cc784788c4729a2c1d4f7713077
atullya <atulmzn1@gmail.com>
2025-12-25 14:44:41 +0545

    baseQuery

--- lib/store/axiosClient.ts
+++ lib/store/axiosClient.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 import { store } from "./store";
 import { logout, refreshTokenSuccess } from "./slices/auth/authSlice";
-// import store from "@/lib/store"; // your Redux store
-// import { logout, refreshTokenSuccess } from "./authSlice";
 
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const axiosClient = axios.create({
   baseURL: API_BASE_URL,
